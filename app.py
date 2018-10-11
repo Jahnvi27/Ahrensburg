90b1823f63090f86513edc1f258ce9439dcd851b
Jahnvi Rai <jahnvi@jahnvis-mbp.mobile.asu.edu>
2018-10-10 18:47:53 -0700

    US#19TASK#31-Link dialogflow with codebase to detect specific intents.Added the json to access dialogflow. Wrote a function to detect intents and another route to which the usersresponse will be submitted

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, request,jsonify, render_template
+import dialogflow
+import os
 
 app = Flask(__name__)
 
@@ -11,3 +13,31 @@ def fetchui():
 
 if __name__ == '__main__':
     app.run()
+
+# method to detect intent based on text
+
+def detect_intent(project_id, session_id, texts, language_code):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+
+    for text in texts:
+        text_input = dialogflow.types.TextInput(
+            text=text, language_code=language_code)
+
+        query_input = dialogflow.types.QueryInput(text=text_input)
+
+        response = session_client.detect_intent(
+            session=session, query_input=query_input)
+        return response.query_result.fulfillment_text
+
+
+# api route to which when the user enter his comment it should be submitted. This method will be called from the js file
+# when the user enters his query
+@app.route('/send_message', methods=['POST'])
+def send_message():
+    message = request.form['message']
+    project_id = os.getenv('DIALOGFLOW_PROJECT_ID')
+    fulfillment_text = detect_intent(project_id, 1, message, 'en')
+    response_text = { "message":  fulfillment_text }
+    return jsonify(response_text)
+
