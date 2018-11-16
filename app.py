259caa141886f8a78d37cfae9f02f50a694c2a68
Jahnvi Rai <jahnvi@Jahnvis-MacBook-Pro.local>
2018-11-15 20:02:02 -0700

    US#67TASK#73-Sprint Modify the webhook to return associated images and videos for tv-shows

--- app.py
+++ app.py
@@ -69,6 +69,20 @@ def fetch_filter_details():
     return jsonify(response_text)
 
 
+@app.route('/fetch_video_url', methods=['GET'])
+def get_video_details(show_id):
+    api_key = os.getenv('TMDB_API_KEY')
+    video_info = requests.get("https://api.themoviedb.org/3/tv/{1}/videos?api_key={0}".format(api_key, show_id),)
+    video_details = video_info.json()
+    if len(video_details['results']) > 0:
+        yt_key = video_details['results'][0]['key']
+    else:
+        yt_key = ""
+    yt_url = "https://www.youtube.com/watch?v=" + yt_key
+    response_text = {"message": yt_url}
+    return jsonify(response_text)
+
+
 @app.route('/get_detail', methods=['POST'])
 def get_detail():
     api_key = os.getenv('TMDB_API_KEY')
@@ -158,7 +172,8 @@ def get_detail():
                     final_path = "http://image.tmdb.org/t/p/w185/" + poster_path
                 else:
                     final_path = ""
-                display = name + '##' + overview + "##" + final_path
+                tv_id = show['id']
+                display = name + '##' + str(tv_id) + "##" + overview + "##" + final_path
                 names = names + display + '| '
             reply = {
 
